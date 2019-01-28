b7e5e24e37164042f4925db891583014337bf08c
Cori Beemish <cbeemish@gmail.com>
2019-01-28 15:23:25 -0500

    Updating Section 16-17

--- Section 16/ScoreKeeper/scorekeeper.js
+++ Section 16/ScoreKeeper/scorekeeper.js
@@ -5,6 +5,7 @@ var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var numInput = document.querySelector("input");
 var winningScoreDisplay = document.querySelector("p span");
+var winner = document.querySelector("h2");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -13,10 +14,12 @@ var winningScore = 5;
 p1Button.addEventListener("click", function(){
 	if(!gameOver){
 		p1Score++;
-		if(p1Score === winningScore){
-			p1Display.classList.add("winner");
-			gameOver = true;
-		}
+        if (p1Score === winningScore) {
+            
+            p1Display.classList.add("winner");
+            gameOver = true;
+            
+        }
 		p1Display.textContent = p1Score;
 	}
 });
@@ -25,9 +28,9 @@ p2Button.addEventListener("click", function(){
 	if(!gameOver){
 		p2Score++;
 		if(p2Score === winningScore){
-			p2Display.classList.add("winner");
+            p2Display.classList.add("winner");
 			gameOver = true;
-		}
+        }
 		p2Display.textContent = p2Score;
 	}
 });
@@ -46,7 +49,7 @@ function reset(){
 	gameOver = false;
 }
 
-numInput.addEventListener("change", function(){
+numInput.addEventListener("change", function () {
 	winningScoreDisplay.textContent = this.value;
 	winningScore = Number(this.value);
 	reset();
